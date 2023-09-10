8f9f70e9b31e218f788303e7057ad91bb6f7a656
Jiei-Dux <Jiei_Dux@proton.me>
2023-09-10 22:19:49 +0800

    Update

--- DBN.sh
+++ DBN.sh
@@ -44,17 +44,27 @@ do
 
 
 
-	if [ "$STATE" -eq 1 ]; then
-		if [ "$PCT" -le 20 ]; then
-			notify-send --app-name "ATTENTION!!!" -t 2000 -u normal "Battery Low. Plug the Charger!"
-			play "/usr/share/sounds/Oxygen-Sys-App-Error-Serious.ogg"
-		fi
-	elif [ "$STATE" -eq 0 ]; then
-		if [ "$PCT" -ge 85 ]; then
-			notify-send --app-name "ATTENTION!!!" -t 2000 -u normal "Battery Full. Unplug the Charger!"
-			play "/usr/share/sounds/Niko\ Niko\ Nii\ SFX.ogg"
-		fi
-	fi
+	#if [ "$STATE" -eq 1 ]; then
+	#	if [ "$PCT" -le 20 ]; then
+	#		notify-send --app-name "ATTENTION!!!" -t 2000 -u normal "Battery Low. Plug the Charger!"
+	#		play "/usr/share/sounds/Oxygen-Sys-App-Error-Serious.ogg"
+	#	fi
+	#elif [ "$STATE" -eq 0 ]; then
+	#	if [ "$PCT" -ge 85 ]; then
+	#		notify-send --app-name "ATTENTION!!!" -t 2000 -u normal "Battery Full. Unplug the Charger!"
+	#		play "/usr/share/sounds/Niko\ Niko\ Nii\ SFX.ogg"
+	#	fi
+	#fi
+
+  if [[ "$STATE" -eq 1 && "$PCT" -le 20 ]]; then
+    notify-send --app-name "Dux's Battery Notfier" -t 2000 -u normal "Battery Low. Plug the Charger!"
+    play "/usr/share/sounds/Oxygen-Sys-App-Error-Serious.ogg"
+  fi
+
+  if [[ "$STATE" -eq 0 && "$PCT" -ge 85 ]]; then
+    notify-send --app-name "Dux's Battery Notifier" -t 2000 -u normal "Battery Full. Unplug the Charger!"
+	  play "/usr/share/sounds/Niko\ Niko\ Nii\ SFX.ogg"
+  fi
 	
 	sleep 10
 done
