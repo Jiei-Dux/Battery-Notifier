d1fa3c5b72b04648729f5e33919a815b88fbb9ee
Jiei-Dux <Jiei_Dux@proton.me>
2023-12-26 16:49:39 +0800

    Update

--- DBN.sh
+++ DBN.sh
@@ -2,7 +2,7 @@
 
 # || WARNING: THE CODE BELOW KILLS DUPLICATE INSTANCES || #
 
-for pid in $(pgrep -f $0);
+for pid in $(pgrep -f DBN.sh);
 do
   if [ $pid != $$ ]; then
     kill $pid
@@ -14,48 +14,85 @@ done
 
 
 
+# ~ YOU CAN EDIT THE CODE BELOW ~ #
+
+# THRESHOLDS:
+LOW_BATTERY_THRESHOLD=20
+CRIT_BATTERY_THRESHOLD=10
+FULL_BATTERY_THRESHOLD=85
+
+# ~ YOU CAN EDIT THE CODE ABOVE ~ #
+
+
+
+
+# * Variables Below * #
+
+BATTERY_PATH="/sys/class/power_supply/*/capacity"
+STATUS_PATH="/sys/class/power_supply/*/status"
+
+PERCENTAGE=$(cat $BATTERY_PATH)
+STATE=$(cat $STATUS_PATH)
+
+sendNotification() {
+  notify-send --app-name "Dux's Battery Notifier" -t 2000 -u normal "$1"
+}
+
+# * Variables Above * #
+
+
+
+
+# || WARNING: THE CODES BELOW HANDLES ERRORS || #
+
+if ! command -v play &> /dev/null; then
+  sendNotification "Error: 'play' command cannot be found... :3"
+  exit 1
+fi
+
+if ! command -v systemctl &> /dev/null; then
+  sendNotification "Error: 'systemctl' command cannot be found... :3"
+  exit 1
+fi
+
+
+
+
+if [ ! -r $BATTERY_PATH ]; then
+  sendNotification "Error: Cannot read battery capacity file... :3"
+  exit 1
+fi
+
+if [ ! -r $STATUS_PATH ]; then
+  sendNotification "Error: Cannot read battery status file... :3"
+  exit 1
+fi
+
+# || WARNING: THE CODES ABOVE HANDLES ERRORS || #
+
+
+
+
+# || WARNING: DO NOT EDIT THE CODE BELOW || #
+
 while true
 do
-  # ~ YOU CAN EDIT THE CODE BELOW ~ #
 
-  # NOTE:
-  # you can check your device name using "upower -d"
-  # as well as use "*" if you're too lazy.
-  DEVICE_NAME='battery_BAT1'
-
-  # ~ YOU CAN EDIT THE CODE ABOVE ~ #
-
-
-
-
-  # || WARNING: DO NOT EDIT THE CODE BELOW || #
-
-  LOCATION='/org/freedesktop/UPower/devices'
-  DEVICE_LOCATION="$LOCATION/$DEVICE_NAME"
-
-  CMD=$(upower -i "$DEVICE_LOCATION")
-
-  PCT=$(awk '{gsub("%","")} /percentage/ {printf "%s\n", $NF}' <<< "$CMD")
-  STATE=$(awk '/state/ {print $NF}' <<< "$CMD" | grep -c "discharging")
-
-  # || WARNING: DO NOT EDIT THE CODE ABOVE || #
-
-
-
-
-  if [[ "$STATE" -eq 1 && "$PCT" -le 10 ]]; then
+  if [[ $STATE -eq 1 && $PERCENTAGE -le $CRIT_BATTERY_THRESHOLD ]]; then
     systemctl suspend
   fi
 
-  if [[ "$STATE" -eq 1 && "$PCT" -le 20 ]]; then
-    notify-send --app-name "Dux's Battery Notfier" -t 2000 -u normal "Battery Low. Plug the Charger!"
+  if [[ $STATE -eq 1 && $PERCENTAGE -le $LOW_BATTERY_THRESHOLD ]]; then
+    sendNotification "Battery Low. Plug the Charger!"
     play "/usr/share/sounds/Oxygen-Sys-App-Error-Serious.ogg"
   fi
 
-  if [[ "$STATE" -eq 0 && "$PCT" -ge 85 ]]; then
-    notify-send --app-name "Dux's Battery Notifier" -t 2000 -u normal "Battery Full. Unplug the Charger!"
+  if [[ $STATE -eq 0 && $PERCENTAGE -ge $FULL_BATTERY_THRESHOLD ]]; then
+    sendNotification "Battery Full. Unplug the Charger!"
     play "/usr/share/sounds/Niko-Niko-Nii-SFX.ogg"
   fi
 
   sleep 10
 done
+
+# || WARNING: DO NOT EDIT THE CODE ABOVE || #
