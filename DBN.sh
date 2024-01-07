047e1e4ac4bf890247f1a5c8702cac90f74d209f
Jiei-Dux <Jiei_Dux@proton.me>
2024-01-07 17:39:14 +0800

    Update

--- DBN.sh
+++ DBN.sh
@@ -18,77 +18,6 @@ done
 
 
 
-# THRESHOLDS #
-
-critical_battery_threshold=15
-low_battery_threshold=25
-full_battery_threshold=85
-
-# THRESHOLDS #
-
-
-
-
-
-
-
-
-# || WARNING: DO NOT EDIT THE CODE BELOW || #
-
-# VARIABLES #
-
-LOG_FILE="/home/*/.log/battery-notifier/battery_notifier_log.txt"
-
-sendNotification() {
-  notify-send --app-name "DBN - Dux's Battery Notifier" -t 4000 -u normal "$1"
-}
-
-debugNotification() {
-  notify-send --app-name "Debug Notif" -t 4000 -u normal "$1"
-}
-
-logMessage() {
-  echo "$(date '+%Y-%m-%d %I:%M:%S') $1" >> $LOG_FILE
-}
-
-# sendNotification "YEET"
-# debugNotification "YEET"
-
-# logMessage "YEET"
-# cat $LOG_FILE
-
-
-
-
-SOUND_FILE_LOCATION="/usr/share/sounds/"
-
-# play $SOUND_FILE_LOCATION/Oxygen-Sys-App-Error-Serious.ogg
-# play $SOUND_FILE_LOCATION/Niko-Niko-Nii-SFX.ogg
-
-
-
-
-DEVICE_NAME=$(upower -e | grep 'battery' | sed 's/.*\/\(.*\)$/\1/')
-
-LOCATION='/org/freedesktop/UPower/devices'
-BAT_LOCATION="$LOCATION/$DEVICE_NAME"
-
-CMD=$(upower -i "$BAT_LOCATION")
-
-PCT=$(awk '{gsub("%","")} /percentage/ {printf "%s\n", $NF}' <<< "$CMD")
-STATE=$(awk '/state/ {print $NF}' <<< "$CMD" | grep -c "discharging")
-
-# echo $DEVICE_NAME
-# echo $BAT_LOCATION
-# echo $CMD
-# echo $PCT
-# echo $STATE
-
-# VARIABLES #
-
-
-
-
 # || WARNING: THE CODES BELOW HANDLES ERRORS || #
 #:<<COMMENT
 
@@ -116,20 +45,97 @@ fi
 
 
 
+# || WARNING: DO NOT EDIT THE CODE BELOW || #
+
+# ~ VARIABLES ~ #
+
+LOG_FILE="/home/*/.log/battery-notifier/battery_notifier_log.txt"
+
+logMessage() {
+  echo "$(date '+%Y-%m-%d %I:%M:%S') $1" >> $LOG_FILE
+}
+
+# logMessage "YEET"
+# cat $LOG_FILE
+
+
+
+
+SOUND_FILE_LOCATION="/home/*/.sounds"
+
+# play $SOUND_FILE_LOCATION/Oxygen-Sys-App-Error-Serious.ogg
+# play $SOUND_FILE_LOCATION/Niko-Niko-Nii-SFX.ogg
+
+sendNotification() {
+  notify-send --app-name "DBN - Dux's Battery Notifier" -t 4000 -u normal "$1"
+}
+
+debugNotification() {
+  notify-send --app-name "Debug Notif" -t 4000 -u normal "$1"
+}
+
+# sendNotification "YEET"
+# debugNotification "YEET"
+
+# ~ VARIABLES ~ #
+
+# || WARNING: DO NOT EDIT THE CODE ABOVE || #
+
+
+
+
+
+
+
+
+# THRESHOLDS #
+
+critical_battery_threshold=15
+low_battery_threshold=25
+full_battery_threshold=85
+
+# THRESHOLDS #
+
+
+
+
+
+
+
+
 # :<<COMMENT
 while true
 do
 
-  if [[ "$STATE" -eq 1 && "$PCT" -le $CBT ]]; then
+  DEVICE_NAME=$(upower -e | grep 'battery' | sed 's/.*\/\(.*\)$/\1/')
+
+  LOCATION='/org/freedesktop/UPower/devices'
+  BAT_LOCATION="$LOCATION/$DEVICE_NAME"
+
+  CMD=$(upower -i "$BAT_LOCATION")
+
+  PCT=$(awk '{gsub("%","")} /percentage/ {printf "%s\n", $NF}' <<< "$CMD")
+  STATE=$(awk '/state/ {print $NF}' <<< "$CMD" | grep -c "discharging")
+
+  # echo $DEVICE_NAME
+  # echo $BAT_LOCATION
+  # echo $CMD
+  # echo $PCT
+  # echo $STATE
+
+
+
+
+  if [[ "$STATE" -eq 1 && "$PCT" -le $critical_battery_threshold ]]; then
     systemctl suspend
   fi
 
-  if [[ "$STATE" -eq 1 && "$PCT" -le $LBT ]]; then
+  if [[ "$STATE" -eq 1 && "$PCT" -le $low_battery_threshold ]]; then
     sendNotification "Battery Low. Plug the Charger!"
     play "$SOUND_FILE_LOCATION/Oxygen-Sys-App-Error-Serious.ogg"
   fi
 
-  if [[ "$STATE" -eq 0 && "$PCT" -ge $FBT ]]; then
+  if [[ "$STATE" -eq 0 && "$PCT" -ge $full_battery_threshold ]]; then
     sendNotification "Battery Full. Unplug the Charger!"
     play "$SOUND_FILE_LOCATION/Niko-Niko-Nii-SFX.ogg"
   fi
